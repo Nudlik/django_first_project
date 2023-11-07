776b256f069f843bc89d08e6a25ecf74fd92d6ae
Nudlik <gegorp@mail.com>
2023-11-07 10:04:04 +0700

    add "products"

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+
 from django.db import models
 
 # Create your models here.
+
+
+@dataclass(frozen=True)
+class Product:
+    id: int
+    name: str
+    description: list
+    price: float
