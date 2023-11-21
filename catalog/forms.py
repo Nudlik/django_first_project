63330f403b5159cd27f7191433c96f3734681e12
Nudlik <gegorp@mail.com>
2023-11-21 18:34:13 +0700

    рефактор страницы с добавлением продукта

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,11 +3,27 @@ from .models import Category
 
 
 class AddProductForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Название')
-    description = forms.CharField(widget=forms.Textarea,
+    title = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                          'placeholder': 'Название продукта',
+                                                                          }), label='Название')
+    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 50,
+                                                               'rows': 5,
+                                                               'class': 'form-control',
+                                                               'placeholder': 'Вводите данные с разделителем ";"',
+                                                               }
+                                                        ),
                                   required=False,
                                   label='Описание',
-                                  help_text='Вводите данные с разделителем ";"')
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория')
-    price = forms.DecimalField(max_digits=12, decimal_places=2, min_value=0, label='Стоимость')
-    is_published = forms.BooleanField(label='Опубликовано')
+                                  )
+    category = forms.ModelChoiceField(queryset=Category.objects.all(),
+                                      empty_label='Категория не выбрана',
+                                      label='Категория',
+                                      widget=forms.Select(attrs={'class': 'form-control'}),
+                                      )
+    price = forms.DecimalField(max_digits=12,
+                               decimal_places=2,
+                               min_value=0,
+                               label='Стоимость',
+                               widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '0.00'})
+                               )
+    is_published = forms.BooleanField(initial=True, label='Опубликовано')
