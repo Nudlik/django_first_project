bcd95b97b9bb132098f0100329eb5c5d8bc5d6bc
Nudlik <gegorp@mail.com>
2023-11-29 08:16:01 +0700

    вынес повторяющийся код в миксин

--- catalog/utils.py
+++ catalog/utils.py
@@ -78,3 +78,35 @@ def format_to_row(products: list, count_rows: int = 4) -> list:
                 row.append(products[j])
             res.append(row)
     return res
+
+
+menu = [
+    {'title': 'Главная', 'url_name': 'home'},
+    {'title': 'Каталог', 'url_name': 'catalog'},
+    {'title': 'Категории', 'url_name': 'category'},
+    {'title': 'Добавить продукт', 'url_name': 'add_product'},
+    {'title': 'Контакты', 'url_name': 'contacts'},
+]
+
+
+class MenuMixin:
+    page_title: str = None
+    page_description: str = None
+
+    def get_mixin_context(self, context: dict, **kwargs) -> dict:
+        context.update(kwargs)
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.page_title is not None:
+            context['title'] = self.page_title
+
+        if self.page_description is not None:
+            context['description'] = self.page_description
+
+        if 'menu' not in context:
+            context['menu'] = menu
+
+        return context
--- catalog/views.py
+++ catalog/views.py
@@ -1,64 +1,45 @@
 from django.core.paginator import Paginator
 from django.db.models import Sum, Count
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views import View
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
 from .forms import AddProductForm
-from .models import Product, Category
-
-menu = [
-    {'title': 'Главная', 'url_name': 'home'},
-    {'title': 'Каталог', 'url_name': 'catalog'},
-    {'title': 'Категории', 'url_name': 'category'},
-    {'title': 'Добавить продукт', 'url_name': 'add_product'},
-    {'title': 'Контакты', 'url_name': 'contacts'},
-]
+from .models import Product, Category, Contact
+from .utils import MenuMixin
 
 
-class IndexTemplateView(TemplateView):
+class IndexTemplateView(MenuMixin, TemplateView):
     template_name = 'catalog/index.html'
-    extra_context = {
-        'title': 'Главная страница',
-        'description': 'Skystore - это интернет-магазин',
-        'menu': menu
-    }
+    page_title = 'Главная страница'
+    page_description = 'Skystore - это интернет-магазин'
 
 
-class ProductListView(ListView):
+class ProductListView(MenuMixin, ListView):
     template_name = 'catalog/catalog.html'
     context_object_name = 'products'
     paginate_by = 6
-    extra_context = {
-        'title': 'Каталог',
-        'description': 'Skystore - это отличный вариант хранения ваших вещей '
-                       'и примеров товаров, который вы бы хотели продать',
-        'menu': menu,
-    }
+    page_title = 'Каталог'
+    page_description = ('Skystore - это отличный вариант хранения ваших вещей '
+                        'и примеров товаров, который вы бы хотели продать')
 
     def get_queryset(self):
         return Product.published.all().order_by('-time_update').select_related('category')
 
 
-class ContactsView(View):
-    data = {
-        'title': 'Контакты',
-        'description': 'Мы всегда рады обратной связи',
-        'menu': menu,
-    }
-
-    def get(self, request):
-        return render(request, 'catalog/contacts.html', context=self.data)
+class ContactsView(MenuMixin, ListView):
+    model = Contact
+    template_name = 'catalog/contacts.html'
+    page_title = 'Контакты'
+    page_description = 'Мы всегда рады обратной связи'
 
     def post(self, request):
         form_data = request.POST
         [print(i, form_data.get(i)) for i in ('name', 'phone', 'message')]
-        return render(request, 'catalog/contacts.html', context=self.data)
+        return self.get(request)
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(MenuMixin, DetailView):
     template_name = 'catalog/product.html'
     pk_url_kwarg = 'product_id'
 
@@ -67,22 +48,19 @@ class ProductDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = f'Страница с описанием продукта: {self.object.title}'
-        context['menu'] = menu
-        return context
+        return self.get_mixin_context(context,
+                                      title=f'Страница с описанием продукта: {self.object.title}',
+                                      )
 
     def post(self, request, product_id):
         print(f'Пользователь нажал на кнопку купить, товар с id: {self.request.POST.get("button_buy")}')
         return self.get(request)
 
 
-class CategoryListView(ListView):
+class CategoryListView(MenuMixin, ListView):
     template_name = 'catalog/category.html'
-    extra_context = {
-        'title': 'Категории',
-        'description': 'Таблица всех категорий продуктов на сайте',
-        'menu': menu,
-    }
+    page_title = 'Категории'
+    page_description = 'Таблица всех категорий продуктов на сайте'
 
     def get_queryset(self):
         return Category.objects.annotate(total_products=Count('products'),
@@ -90,7 +68,7 @@ class CategoryListView(ListView):
                                          ).order_by('pk')
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(MenuMixin, DetailView):
     template_name = 'catalog/list_category.html'
     pk_url_kwarg = 'category_id'
 
@@ -103,23 +81,19 @@ class CategoryDetailView(DetailView):
         category = self.get_object()
         products = Product.objects.filter(category=category).select_related('category')
         paginator = Paginator(products, per_page=3)
-        page_id = self.request.GET.get('page')
-        page_obj = paginator.get_page(page_id)
+        page_num = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_num)
 
-        context['title'] = f'Категория: {self.object.title}'
-        context['menu'] = menu
-        context['paginator'] = paginator
-        context['page_obj'] = page_obj
-        return context
+        return self.get_mixin_context(context,
+                                      title=f'Категория: {self.object.title}',
+                                      paginator=paginator,
+                                      page_obj=page_obj
+                                      )
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(MenuMixin, CreateView):
     form_class = AddProductForm
     template_name = 'catalog/add_product.html'
     success_url = reverse_lazy('catalog')
-    extra_context = {
-        'title': 'Добавить продукт',
-        'description': 'Здесь можно добавить новый продукт, чтобы он появился на сайте',
-        'menu': menu,
-    }
-
+    page_title = 'Добавить продукт'
+    page_description = 'Здесь можно добавить новый продукт, чтобы он появился на сайте'
